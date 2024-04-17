1e4467e23b79c402dcead5530ccf21eb806ee715
yaserxm <yaserxm@172.20.10.2>
2024-04-17 08:43:33 +0300

    first one

--- test/studentgrade/StudentGradeTest.java
+++ test/studentgrade/StudentGradeTest.java
@@ -1,12 +1,33 @@
 package studentgrade;
 
 import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 /* JUnit 3.8.1 */
 public class StudentGradeTest extends TestCase {
     
     public StudentGradeTest(String testName) {
         super(testName);
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
     }
 
     public void testGetGrade95() {
@@ -31,5 +52,31 @@ public class StudentGradeTest extends TestCase {
         String expResult = "F";
         String result = StudentGrade.getGrade(mark);
         assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of main method, of class StudentGrade.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        StudentGrade.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getGrade method, of class StudentGrade.
+     */
+    @Test
+    public void testGetGrade() {
+        System.out.println("getGrade");
+        int mark = 0;
+        String expResult = "";
+        String result = StudentGrade.getGrade(mark);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
     }
 }
